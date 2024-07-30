c40387136de6d35ec8d7fec1703495b418eed927
em-3 <69275008+em-3@users.noreply.github.com>
2024-07-29 20:12:37 -0400

    Added perfectionist plugin for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,65 @@
 import js from "@eslint/js";
-import ts from "typescript-eslint";
-import svelte from "eslint-plugin-svelte";
 import prettier from "eslint-config-prettier";
+import perfectionist from "eslint-plugin-perfectionist";
+import svelte from "eslint-plugin-svelte";
 import globals from "globals";
+import ts from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs["flat/recommended"],
 	prettier,
+	...svelte.configs["flat/recommended"],
 	...svelte.configs["flat/prettier"],
+	{
+		plugins: {
+			perfectionist
+		},
+		rules: {
+			"perfectionist/sort-imports": [
+				"error",
+				{
+					type: "alphabetical",
+					order: "asc",
+					ignoreCase: true,
+					internalPattern: ["~/**"],
+					newlinesBetween: "always",
+					maxLineLength: undefined,
+					groups: [
+						"type",
+						["builtin", "external"],
+						"internal-type",
+						"internal",
+						["parent-type", "sibling-type", "index-type"],
+						["parent", "sibling", "index"],
+						"object",
+						"unknown"
+					],
+					customGroups: { type: {}, value: {} },
+					environment: "node"
+				}
+			],
+			"perfectionist/sort-exports": [
+				"error",
+				{
+					type: "alphabetical",
+					order: "asc",
+					ignoreCase: true
+				}
+			],
+			"perfectionist/sort-svelte-attributes": [
+				"error",
+				{
+					type: "alphabetical",
+					order: "asc",
+					ignoreCase: true,
+					groups: [],
+					customGroups: {}
+				}
+			]
+		}
+	},
 	{
 		languageOptions: {
 			globals: {
