4a5b3ff0faabe9ccc129c00f3bfbba550efd3c47
em-3 <69275008+em-3@users.noreply.github.com>
2024-07-29 20:17:58 -0400

    Added a setup screen to select the app backend

---
+++ src/routes/setup/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import BackendCard from "./BackendCard.svelte";
+</script>
+
+<div class="fade-in flex size-full flex-col items-center justify-center gap-10">
+	<div class="text-center">
+		<h1 class="text-4xl font-bold text-black dark:text-white">Let's Get Started</h1>
+		<h2 class="text-2xl text-black dark:text-white">Choose how you will access Jira</h2>
+	</div>
+
+	<div class="flex items-center justify-center gap-10">
+		<BackendCard
+			description="No setup required on the Jira server. Requires additional configuration on the client. Does not last between sessions."
+			dest="setup/token"
+			icon="ph:desktop-tower-duotone"
+			recommended
+			service="Browser Token"
+		></BackendCard>
+	</div>
+</div>
