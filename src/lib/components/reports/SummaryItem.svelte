09afed2c296ec7f36b7b1f650046b4158da905e4
em-3 <69275008+em-3@users.noreply.github.com>
2024-12-18 15:17:21 -0500

    Added a subtitle option to SummaryItem

--- src/lib/components/reports/SummaryItem.svelte
+++ src/lib/components/reports/SummaryItem.svelte
@@ -1,10 +1,13 @@
 <script>
-    let { total, name } = $props();
+    let { total, name, subtitle = undefined } = $props();
 </script>
 
 <div>
     <h3>{total}</h3>
-    <p>{name}</p>
+    <h4>{name}</h4>
+    {#if subtitle}
+    <p>{subtitle}</p>
+    {/if}
 </div>
 
 <style>
@@ -16,6 +19,7 @@
         padding: 20px;
         background-color: var(--secondary-background-color);
         border-radius: 25px;
+        gap: 5px;
     }
 
     h3 {
