46a1aad8db105d17990bccd73e96933f266df8ef
em-3 <69275008+em-3@users.noreply.github.com>
2024-12-18 20:09:48 -0500

    Added missing return values for formattedTime.ts

--- src/lib/formattedTime.ts
+++ src/lib/formattedTime.ts
@@ -1,18 +1,18 @@
 /**
  * Converts the given number of seconds into hours
- * @param {number} seconds The value to format
+ * @param seconds The value to format
  * @returns The number of hours (rounded down)
  */
-function formatHours(seconds: number) {
+function formatHours(seconds: number): number {
     return Math.floor(seconds / 60 / 60);
 }
 
 /**
  * Formats the given number of seconds into hour-minute format
- * @param {number} seconds The value to format
+ * @param seconds The value to format
  * @returns A string in the format `{hours}h {minutes}m`
  */
-function formatHoursMinutes(seconds: number) {
+function formatHoursMinutes(seconds: number): string {
     let hours = Math.floor(seconds / 60 / 60);
     let minutes = Math.floor(seconds / 60 % 60);
 
