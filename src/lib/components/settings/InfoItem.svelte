d44c113943665986ee0363c33a7a3f43fb1a0260
em-3 <69275008+em-3@users.noreply.github.com>
2024-12-22 14:45:54 -0500

    Created InfoItem component

---
+++ src/lib/components/settings/InfoItem.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    interface InfoItemProperties {
+        name: string,
+        Icon: any,
+        children: Snippet
+    }
+
+    let { name, Icon = undefined, children }: InfoItemProperties = $props();
+</script>
+
+<section class="info-item">
+    <header>
+        <h3>
+            <Icon />
+            <span>{ name }</span>
+        </h3>
+    </header>
+    <section class="items">
+        {@render children()}
+    </section>
+</section>
+
+<style>
+    h3 {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        font-size: 1em;
+    }
+
+    .info-item {
+        display: contents;
+    }
+
+    .items {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+</style>
