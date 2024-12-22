df2e61d44649f26737b698c179cae687e0d95457
em-3 <69275008+em-3@users.noreply.github.com>
2024-12-22 14:46:13 -0500

    Created SettingsCategory component

---
+++ src/lib/components/settings/SettingsCategory.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import RoundedSection from "../RoundedSection.svelte";
+
+    let { name, Icon = undefined, children }: { name: string, Icon: any, children: Snippet } = $props();
+</script>
+
+<RoundedSection title={name} {Icon}>
+    <div class="settings">
+        {@render children()}
+    </div>
+</RoundedSection>
+
+<style>
+    .settings {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 20px;
+        align-items: center;
+    }
+
+    @media (min-width: 64em) {
+        .settings {
+            grid-template-columns: 1fr 2fr;
+        }
+    }
+</style>
