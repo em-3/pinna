a5b6ef8a6b5be1e8699251b1307c44d91e5a3a2f
em-3 <69275008+em-3@users.noreply.github.com>
2024-12-27 16:04:03 -0500

    Fixed settings not loading properly on refresh

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,6 +5,6 @@ import { loadConfig } from "$lib/config.svelte";
 // Force SvelteKit to prerender
 export const prerender = true;
 export const ssr = false;
-export const load: PageLoad = () => {
-    loadConfig();
+export const load: PageLoad = async () => {
+    await loadConfig();
 };
