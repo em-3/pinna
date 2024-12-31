b35d712ff2f44b28066ef5764c7ca806f890a9a1
em-3 <69275008+em-3@users.noreply.github.com>
2024-12-31 13:26:18 -0500

    Renamed svelte.config.ts to svelte.config.js

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,7 @@ import { loadConfig } from "$lib/stores/config.svelte";
 // Force SvelteKit to prerender
 export const prerender = true;
 export const ssr = false;
+
 export const load: PageLoad = async () => {
     await loadConfig();
 };
--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,8 @@
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 import adapter from "@sveltejs/adapter-static";
-import { Config } from "@sveltejs/kit";
 
-const config: Config = {
+const config = {
   kit: {
     adapter: adapter(),
   },
